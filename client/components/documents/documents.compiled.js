443333eec51be88d95fdee934599b534f9919940
Joe Prempeh <prempehjoe@gmail.com>
2015-07-13 17:16:15 -0700

    chore(deploy): complied file added

---
+++ client/components/documents/documents.compiled.js
@@ -0,0 +1,129 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = {
+	key: '791996101d4f11e5ae6f89ed27dee555'
+};
+},{}],2:[function(require,module,exports){
+(function (process){
+
+
+var config = (process.env.TRAVIS) ? process.env.config :require('./config.js');
+$(function() {
+	$('#upload-form').transloadit({
+		// We want to wait for all encodings to finish before the form
+		// is submitted.veaea
+		wait: true,
+		// The upload to Transloadit should start as soon as the user
+		// selects some files.
+		triggerUploadOnFileSelection: false,
+		autoSubmit: false,
+		params: {
+			auth: {
+				// This is your API key.
+				key: process.env.TRANSLOADIT || config.key
+			},
+			template_id: '275df800240e11e5a20c2dc0bd276748'
+		},
+		onSuccess: function(assembly) {
+			var thisTeam = localStorage['$selectedteam'];
+			var thisUser = localStorage['firebase:session::mksequip'].uid;
+			var refUrl = 'https://mksequip.firebaseIO.com/';
+			var fb = new Firebase(refUrl);
+			fb = fb.child('teams').child(window.selectedTeam).child('images').child(assembly.assembly_id);
+			fb.set(assembly.results);
+		}
+	});
+});
+}).call(this,require('_process'))
+},{"./config.js":1,"_process":3}],3:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = setTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            currentQueue[queueIndex].run();
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    clearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        setTimeout(drainQueue, 0);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+},{}]},{},[2]);

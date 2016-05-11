cbd0deeadf4808744eb3c917b635e7b2199ade4c
Joe Prempeh <prempehjoe@gmail.com>
2016-05-11 14:38:37 -0700

    loads environmental config

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var morgan = require('morgan');
+var logger = require('winston');
+var path = require('path');
+var db = require('../db/index.js');
+
+require('dotenv').config({path: path.join(__dirname, '../.env')});
 
 var app = express();
 var port = process.env.PORT || 3000;
+app.use(morgan('dev'));
 
 app.set('port', port);
 
 app.use(express.static(__dirname + '/../client'));
 
-var server = app.listen(port, function () {
-	console.log('Equip listening at http://localhost:%s', port);
+app.listen(port, function () {
+	logger.info('Equip listening at ' + process.env.HOST + ':' + port);
 });
